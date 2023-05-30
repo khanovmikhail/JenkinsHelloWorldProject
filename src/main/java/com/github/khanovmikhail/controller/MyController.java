de84926c48625a8a71ea26196e365b0a1e9ca53d
khanovmikhail <johanlgw@gmail.com>
2023-05-30 17:18:30 +0400

    Change Controller to Hello

--- src/main/java/com/github/khanovmikhail/controller/MyController.java
+++ src/main/java/com/github/khanovmikhail/controller/MyController.java
@@ -1,9 +1,7 @@
 package com.github.khanovmikhail.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +10,6 @@ public class MyController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World";
+        return "Hello";
     }
 }
